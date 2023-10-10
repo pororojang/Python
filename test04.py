c70886fc075e3086d26e0f21a719253f4838e2c1
pororojang <147477089+pororojang@users.noreply.github.com>
2023-10-10 13:21:51 +0700

    Add files via upload

---
+++ test04.py
@@ -0,0 +1,3 @@
+Input1 = ["apple","3Banana","Cherry","d4ragonfruit","elderberry"]
+Input2 = ["123","Hello","world","Python","99Programming"]
+Input3 = ['AI',"65Machine","Learning","Deep","Learning101"]
